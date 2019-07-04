9f2dfcc37aa8252153b4a9987ab3bdccb9278907
dgoldstein1 <david.goldstein@deciphernow.com>
2019-07-04 14:07:59 -0500

    got watch changes working

--- crawler_test.go
+++ crawler_test.go
@@ -10,12 +10,15 @@ import (
 func TestCrawl(t *testing.T) {
 	os.Setenv("GRAPH_DB_ENDPOINT", "http://localhost:17474")
 	r, _ := regexp.Compile("\\A/wiki/")
+
 	Crawl("https://en.wikipedia.org/wiki/String_cheese", r, 2)
 }
 
 func TestAddToDb(t *testing.T) {}
 
-func TestConnectToDB(t *testing.T) {}
+func TestConnectToDB(t *testing.T) {
+	
+}
 
 // adopted taken from https://gist.github.com/samalba/6059502
 func AssertEqual(t *testing.T, a interface{}, b interface{}) {
--- watch_dev_changes.sh
+++ watch_dev_changes.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 while true; do
 
-run_tests() {
-	go test $(go list ./... | grep -v /vendor/)
-}
-
 inotifywait -e modify,create,delete -r ./ && \
 	clear
 	go build \
-		&& run_tests
+		&& go test -v
 done
