772dbb8c9044ebfee95bd28ddea111e0a7299c7b
dgoldstein1 <david.goldstein@deciphernow.com>
2019-07-04 14:32:27 -0500

    testing connect to db

--- crawler_test.go
+++ crawler_test.go
@@ -2,10 +2,10 @@ package crawler
 
 import (
 	"testing"
-	// "regexp"
 	"reflect"
 	"os"
 	"github.com/jarcoal/httpmock"
+	"errors"
 )
 
 
@@ -18,9 +18,12 @@ import (
 
 func TestAddToDb(t *testing.T) {}
 
-func TestDoStuffWithTestServer(t *testing.T) {
+func TestConnectToDB(t *testing.T) {
 	dbEndpoint := "http://localhost:17474"
 	os.Setenv("GRAPH_DB_ENDPOINT", dbEndpoint)
+	// first test bad response
+	err := connectToDB()
+	AssertErrorEqual(t, err, errors.New(`Get http://localhost:17474/metrics: dial tcp 127.0.0.1:17474: connect: connection refused`))
 	// mock out http endpoint
 	httpmock.Activate()
 	defer httpmock.DeactivateAndReset()
@@ -28,7 +31,7 @@ func TestDoStuffWithTestServer(t *testing.T) {
 	httpmock.RegisterResponder("GET", dbEndpoint + "/metrics",
 		httpmock.NewStringResponder(200, `[{"id": 1, "name": "My Great Article"}]`))
 	// Use Client & URL from our local test server
-	err := connectToDB()
+	err = connectToDB()
 	AssertErrorEqual(t, err, nil)
 }
 
