f23c2bd200949af8fc0ac95a1f5b4128d5132be6
dgoldstein1 <david.goldstein@deciphernow.com>
2019-07-05 11:37:33 -0500

    able to mock out db for crawl()

--- crawler_test.go
+++ crawler_test.go
@@ -11,34 +11,30 @@ import (
 
 func TestCrawl(t *testing.T) {
 	// function doing setup of tests
-	_resetTests := func(t *testing.T) {
-		httpmock.Activate()
-		defer httpmock.DeactivateAndReset()
-		dbEndpoint := "http://localhost:17474"
-		os.Setenv("GRAPH_DB_ENDPOINT", dbEndpoint)
-		// connect to DB endpoint
-		httpmock.RegisterResponder("GET", dbEndpoint + "/metrics",
-			httpmock.NewStringResponder(200, `TEST`))
-		// add to DB endpoints
-		httpmock.RegisterResponder("GET", dbEndpoint + "/neighbors?node=2",
-			func(req *http.Request) (*http.Response, error) {
-				return httpmock.NewJsonResponse(200, []string{"5","3","7"})
-			},
-		)
-		httpmock.RegisterResponder("POST", dbEndpoint + "/neighbors?node=2",
-			func(req *http.Request) (*http.Response, error) {
-				body := make(map[string][]string)
-				err := json.NewDecoder(req.Body).Decode(&body);
-				if err != nil {
-					t.Error(err)
-				}
-				return httpmock.NewJsonResponse(200, body)
-			},
-		)
-	}
-	// tests
 	t.Run("only filters on links starting with regex", func (t *testing.T)  {
-		_resetTests(t)
+			httpmock.Activate()
+			defer httpmock.DeactivateAndReset()
+			dbEndpoint := "http://localhost:17474"
+			os.Setenv("GRAPH_DB_ENDPOINT", dbEndpoint)
+			// connect to DB endpoint
+			httpmock.RegisterResponder("GET", dbEndpoint + "/metrics",
+				httpmock.NewStringResponder(200, `TEST`))
+			// add to DB endpoints
+			httpmock.RegisterResponder("GET", dbEndpoint + "/neighbors?node=2",
+				func(req *http.Request) (*http.Response, error) {
+					return httpmock.NewJsonResponse(200, []string{"5","3","7"})
+				},
+			)
+			httpmock.RegisterResponder("POST", dbEndpoint + "/neighbors?node=2",
+				func(req *http.Request) (*http.Response, error) {
+					body := make(map[string][]string)
+					err := json.NewDecoder(req.Body).Decode(&body);
+					if err != nil {
+						t.Error(err)
+					}
+					return httpmock.NewJsonResponse(200, body)
+				},
+			)
 		r, _ := regexp.Compile("\\A/wiki/")
 		Crawl("https://en.wikipedia.org/wiki/String_cheese", r, 1)
 	})
