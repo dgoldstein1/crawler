213ac93924e8b5476494cbd4c5bfaec35b7e6c04
dgoldstein1 <david.goldstein@deciphernow.com>
2019-07-09 18:00:01 -0500

    moved wikipedia to its own test case and created crawl_wikipedia.go

--- crawler_test.go
+++ crawler_test.go
@@ -6,29 +6,31 @@ import (
 )
 
 func TestCrawl(t *testing.T) {
-	// function doing setup of tests
-	isValidCrawlLink := func(url string) bool {
-		return strings.HasPrefix(url, "/wiki/") && !strings.Contains(url, ":")
-	}
-
-	nodesAdded := []string{}
-	addEdge := func(currNode string, neighborNode string) (bool, error) {
-		nodesAdded = append(nodesAdded, neighborNode)
-		return false, nil
-	}
-	connectToDB := func() error {return nil}
-	Crawl("https://en.wikipedia.org/wiki/String_cheese", isValidCrawlLink, 2, connectToDB, addEdge)
-
-	t.Run("only filters on links starting with regex", func (t *testing.T)  {
-		for _, url := range nodesAdded {
-			AssertEqual(t, strings.HasPrefix(url, "/wiki/"), true)
+	t.Run("wikipedia", func(t *testing.T) {
+		// function doing setup of tests
+		isValidCrawlLink := func(url string) bool {
+			return strings.HasPrefix(url, "/wiki/") && !strings.Contains(url, ":")
 		}
-	})
-	t.Run("only filters on links starting with regex", func (t *testing.T)  {
-		for _, url := range nodesAdded {
-			if strings.Contains(url, ":") {
-					t.Errorf("Did not expect '%s' to contain ':'", url)
+
+		nodesAdded := []string{}
+		addEdge := func(currNode string, neighborNode string) (bool, error) {
+			nodesAdded = append(nodesAdded, neighborNode)
+			return false, nil
+		}
+		connectToDB := func() error {return nil}
+		Crawl("https://en.wikipedia.org/wiki/String_cheese", isValidCrawlLink, 2, connectToDB, addEdge)
+
+		t.Run("only filters on links starting with regex", func (t *testing.T)  {
+			for _, url := range nodesAdded {
+				AssertEqual(t, strings.HasPrefix(url, "/wiki/"), true)
 			}
-		}
+		})
+		t.Run("only filters on links starting with regex", func (t *testing.T)  {
+			for _, url := range nodesAdded {
+				if strings.Contains(url, ":") {
+					t.Errorf("Did not expect '%s' to contain ':'", url)
+				}
+			}
+		})
 	})
 }
