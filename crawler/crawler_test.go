5f41efba9b8166c0b1f36296fd7bd6d9f37bf6ca
dgoldstein1 <david.goldstein@deciphernow.com>
2019-08-09 16:50:23 -0500

    normal test for run()

--- crawler/crawler_test.go
+++ crawler/crawler_test.go
@@ -1,26 +1,31 @@
 package crawler
 
 import (
-	"fmt"
+	// "fmt"
 	"github.com/stretchr/testify/assert"
-	"os"
-	"strings"
+	// "strings"
 	"testing"
 )
 
 func TestRun(t *testing.T) {
 	// return 'true' if edge already exists
-	edgesAdded := make(map[string][]string)
-	addEdge := func(node string, neighbors []string) ([]string, error) {
-		edgesAdded[node] = neighbors
-		return neighbors, nil
+	nodesAdded := []string{}
+	addEdge := func(currNode string, neighborNodes []string) ([]string, error) {
+		temp := []string{}
+		for _, v := range neighborNodes {
+			temp = append(temp, "https://en.wikipedia.org"+v)
+		}
+		nodesAdded = append(nodesAdded, temp...)
+		return temp, nil
 	}
 	// establishes initial connection to DB
 	connectToDB := func() error { return nil }
 	// check if valid url string for crawling
 	isValidCrawlLink := func(url string) bool { return true }
 	// node2 := "/wiki/node2"
+	newNodeRetrieved := false
 	getNewNode := func() (string, error) {
+		newNodeRetrieved = true
 		return "/wiki/node1", nil
 	}
 
@@ -28,21 +33,21 @@ func TestRun(t *testing.T) {
 		Name             string
 		StartingEndpoint string
 		MaxNodes         int32
-		MaxtRetries      int32
-		MinNodesAdded    int32
-		MaxNodesAdded    int32
-		MaxRetries       string
+		MaxtRetries      int
+		MinNodesAdded    int
+		MaxNodesAdded    int
+		NewNodeRetrieved bool
 	}
 
 	testTable := []Test{
 		Test{
 			Name:             "starts with endpoint if one is passed",
-			StartingEndpoint: "/wiki/String_cheese",
-			MaxNodes:         1,
+			StartingEndpoint: "https://en.wikipedia.org/wiki/String_cheese",
+			MaxNodes:         100,
 			MaxtRetries:      0,
 			MinNodesAdded:    1,
-			MaxNodesAdded:    25,
-			MaxRetries:       "5",
+			MaxNodesAdded:    1000,
+			NewNodeRetrieved: false,
 		},
 	}
 
@@ -50,9 +55,8 @@ func TestRun(t *testing.T) {
 		// run tests
 		t.Run(test.Name, func(t *testing.T) {
 			// reset everything
-			totalNodesAdded = asyncInt(0)
-			maxDepth = asyncInt(0)
-			os.Setenv("MAX_NEW_NODE_RETRIES", test.MaxRetries)
+			newNodeRetrieved = false
+			nodesAdded = []string{}
 			Run(
 				test.StartingEndpoint,
 				test.MaxNodes,
@@ -62,94 +66,96 @@ func TestRun(t *testing.T) {
 				getNewNode,
 			)
 			// make assertions
-			assert.True(t, test.MinNodesAdded < totalNodesAdded.get())
-			assert.True(t, test.MaxNodesAdded > totalNodesAdded.get())
+			assert.True(t, test.MinNodesAdded < len(nodesAdded))
+			assert.True(t, test.MaxNodesAdded > len(nodesAdded))
+			assert.Equal(t, test.NewNodeRetrieved, newNodeRetrieved)
 		})
 	}
 }
 
-func TestCrawl(t *testing.T) {
-	isValidCrawlLink := func(url string) bool {
-		return strings.HasPrefix(url, "/wiki/") && !strings.Contains(url, ":")
-	}
-	nodesAdded := []string{}
-	addEdges := func(currNode string, neighborNodes []string) ([]string, error) {
-		nodesAdded = append(nodesAdded, neighborNodes...)
-		return neighborNodes, nil
-	}
-	endpoint := "https://en.wikipedia.org/wiki/String_cheese"
-
-	// mock out log.Fatalf
-	originLogPrintf := logMsg
-	defer func() { logMsg = originLogPrintf }()
-	logs := []string{}
-	logMsg = func(format string, args ...interface{}) {
-		if len(args) > 0 {
-			logs = append(logs, fmt.Sprintf(format, args))
-		} else {
-			logs = append(logs, format)
-		}
-	}
-
-	// mute warnings
-	originLogWarn := logWarn
-	defer func() { logWarn = originLogWarn }()
-	logWarn = func(format string, args ...interface{}) {}
-
-	// keep errors in array
-	errors := []string{}
-	logErr = func(format string, args ...interface{}) {
-		if len(args) > 0 {
-			errors = append(errors, fmt.Sprintf(format, args))
-		} else {
-			errors = append(errors, format)
-		}
-	}
-
-	t.Run("works with isValidCrawlLink", func(t *testing.T) {
-		nodesAdded = []string{}
-		// function doing setup of tests
-		Crawl("https://en.wikipedia.org/wiki/String_cheese", 2, isValidCrawlLink, addEdges)
-		t.Run("only filters on links starting with regex", func(t *testing.T) {
-			errors = []string{}
-			for _, url := range nodesAdded {
-				assert.Equal(t, strings.HasPrefix(url, "/wiki/"), true)
-			}
-			assert.Equal(t, []string{}, errors)
-		})
-		t.Run("only filters on links starting with regex", func(t *testing.T) {
-			errors = []string{}
-			for _, url := range nodesAdded {
-				if strings.Contains(url, ":") {
-					t.Errorf("Did not expect '%s' to contain ':'", url)
-				}
-			}
-			assert.Equal(t, []string{}, errors)
-		})
-	})
-
-	t.Run("adds nodes correctly", func(t *testing.T) {
-		nodesAdded = []string{}
-		logs = []string{}
-		errors = []string{}
-		Crawl(
-			endpoint,
-			100,
-			isValidCrawlLink,
-			func(currNode string, neighborNodes []string) ([]string, error) {
-				temp := []string{}
-				for _, v := range neighborNodes {
-					temp = append(temp, "https://en.wikipedia.org"+v)
-				}
-				nodesAdded = append(nodesAdded, temp...)
-				return temp, nil
-			},
-		)
-
-		assert.Equal(t, "starting at ["+endpoint+"]", logs[0])
-		// only add first recursion nodes, ~30,000 on second recursion
-		assert.Equal(t, len(nodesAdded) >= 50, true)
-		assert.Equal(t, len(nodesAdded) <= 30000, true)
-		assert.Equal(t, []string{}, errors)
-	})
-}
+//
+// func TestCrawl(t *testing.T) {
+// 	isValidCrawlLink := func(url string) bool {
+// 		return strings.HasPrefix(url, "/wiki/") && !strings.Contains(url, ":")
+// 	}
+// 	nodesAdded := []string{}
+// 	addEdges := func(currNode string, neighborNodes []string) ([]string, error) {
+// 		nodesAdded = append(nodesAdded, neighborNodes...)
+// 		return neighborNodes, nil
+// 	}
+// 	endpoint := "https://en.wikipedia.org/wiki/String_cheese"
+//
+// 	// mock out log.Fatalf
+// 	originLogPrintf := logMsg
+// 	defer func() { logMsg = originLogPrintf }()
+// 	logs := []string{}
+// 	logMsg = func(format string, args ...interface{}) {
+// 		if len(args) > 0 {
+// 			logs = append(logs, fmt.Sprintf(format, args))
+// 		} else {
+// 			logs = append(logs, format)
+// 		}
+// 	}
+//
+// 	// mute warnings
+// 	originLogWarn := logWarn
+// 	defer func() { logWarn = originLogWarn }()
+// 	logWarn = func(format string, args ...interface{}) {}
+//
+// 	// keep errors in array
+// 	errors := []string{}
+// 	logErr = func(format string, args ...interface{}) {
+// 		if len(args) > 0 {
+// 			errors = append(errors, fmt.Sprintf(format, args))
+// 		} else {
+// 			errors = append(errors, format)
+// 		}
+// 	}
+//
+// 	t.Run("works with isValidCrawlLink", func(t *testing.T) {
+// 		nodesAdded = []string{}
+// 		// function doing setup of tests
+// 		Crawl("https://en.wikipedia.org/wiki/String_cheese", 2, isValidCrawlLink, addEdges)
+// 		t.Run("only filters on links starting with regex", func(t *testing.T) {
+// 			errors = []string{}
+// 			for _, url := range nodesAdded {
+// 				assert.Equal(t, strings.HasPrefix(url, "/wiki/"), true)
+// 			}
+// 			assert.Equal(t, []string{}, errors)
+// 		})
+// 		t.Run("only filters on links starting with regex", func(t *testing.T) {
+// 			errors = []string{}
+// 			for _, url := range nodesAdded {
+// 				if strings.Contains(url, ":") {
+// 					t.Errorf("Did not expect '%s' to contain ':'", url)
+// 				}
+// 			}
+// 			assert.Equal(t, []string{}, errors)
+// 		})
+// 	})
+//
+// 	t.Run("adds nodes correctly", func(t *testing.T) {
+// 		nodesAdded = []string{}
+// 		logs = []string{}
+// 		errors = []string{}
+// 		Crawl(
+// 			endpoint,
+// 			100,
+// 			isValidCrawlLink,
+// func(currNode string, neighborNodes []string) ([]string, error) {
+// 	temp := []string{}
+// 	for _, v := range neighborNodes {
+// 		temp = append(temp, "https://en.wikipedia.org"+v)
+// 	}
+// 	nodesAdded = append(nodesAdded, temp...)
+// 	return temp, nil
+// },
+// 		)
+//
+// 		assert.Equal(t, "starting at ["+endpoint+"]", logs[0])
+// 		// only add first recursion nodes, ~30,000 on second recursion
+// 		assert.Equal(t, len(nodesAdded) >= 50, true)
+// 		assert.Equal(t, len(nodesAdded) <= 30000, true)
+// 		assert.Equal(t, []string{}, errors)
+// 	})
+// }
