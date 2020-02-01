5a96f45f6584ba24f8d959749fd8af46dc180861
dgoldstein1 <david.goldstein@deciphernow.com>
2020-01-31 21:41:29 -0500

    Tests are passing

--- util/util.go
+++ util/util.go
@@ -1,5 +1,33 @@
 package util
 
-func ReadRandomLineFromFile(file string) (string, error) {
-	return "", nil
+import (
+	"bufio"
+	"fmt"
+	log "github.com/sirupsen/logrus"
+	"math/rand"
+	"os"
+	"strings"
+)
+
+var logErr = log.Errorf
+
+func ReadRandomLineFromFile(envName string, baseEndpoint string, prefix string) (string, error) {
+	path := os.Getenv(envName)
+	if path == "" {
+		return "", fmt.Errorf("%s was not set", envName)
+	}
+	// read in file to list of strings
+	file, err := os.Open(path)
+	defer file.Close()
+	if err != nil {
+		return "", err
+	}
+	scanner := bufio.NewScanner(file)
+	words := []string{}
+	for scanner.Scan() {
+		words = append(words, strings.ToLower(scanner.Text()))
+	}
+	err = scanner.Err()
+	// get random index of list
+	return baseEndpoint + prefix + words[rand.Intn(len(words))], err
 }
--- util/util_test.go
+++ util/util_test.go
@@ -1,41 +1,82 @@
 package util
 
 import (
+	"fmt"
 	"github.com/stretchr/testify/assert"
+	"os"
 	"testing"
 )
 
 func TestReadRandomLineFromFile(t *testing.T) {
+	errorsLogged := []string{}
+	logErr = func(format string, args ...interface{}) {
+		if len(args) > 0 {
+			errorsLogged = append(errorsLogged, fmt.Sprintf(format, args))
+		} else {
+			errorsLogged = append(errorsLogged, format)
+		}
+	}
+
 	type Test struct {
 		Name          string
-		File          string
 		ExpectedError string
+		Before        func()
+		After         func()
+		EnvName       string
 	}
+
+	prefix := "/synonym/ar/"
+	baseEndpoint := "https://synonyms.reverso.net"
 
 	defaultTextDir := "../ar_synonyms/arabic.txt"
 	testTable := []Test{
 		Test{
-			Name:          "Bad file",
-			File:          "sdfsdf",
+			Name:          "ARABIC_WORD_LIST_PATH not set",
 			ExpectedError: "ARABIC_WORD_LIST_PATH was not set",
+			EnvName:       "ARABIC_WORD_LIST_PATH",
+			Before: func() {
+				os.Setenv("ARABIC_WORD_LIST_PATH", "")
+			},
+			After: func() {
+				os.Setenv("ARABIC_WORD_LIST_PATH", defaultTextDir)
+			},
 		},
 		Test{
 			Name:          "gets random word succesfully",
-			File:          defaultTextDir,
+			EnvName:       "ARABIC_WORD_LIST_PATH",
 			ExpectedError: "",
+			Before: func() {
+				os.Setenv("ARABIC_WORD_LIST_PATH", defaultTextDir)
+			},
+			After: func() {},
+		},
+		Test{
+			Name:          "no such path",
+			ExpectedError: "open this/does/not/exist: no such file or directory",
+			EnvName:       "ARABIC_WORD_LIST_PATH",
+			Before: func() {
+				os.Setenv("ARABIC_WORD_LIST_PATH", "this/does/not/exist")
+			},
+			After: func() {
+				os.Setenv("ARABIC_WORD_LIST_PATH", defaultTextDir)
+			},
 		},
 	}
 
 	for _, test := range testTable {
 		t.Run(test.Name, func(t *testing.T) {
-			w, err := ReadRandomLineFromFile(test.File)
-			if err != nil {
-				assert.Equal(t, test.ExpectedError, err.Error())
-			} else {
-				assert.Equal(t, test.ExpectedError, "")
+			test.Before()
+			w, err := ReadRandomLineFromFile(test.EnvName, baseEndpoint, prefix)
+			// positive tests
+			if test.ExpectedError == "" {
 				assert.NotEqual(t, "", w)
+				assert.Equal(t, nil, err)
+			} else {
+				assert.Equal(t, "", w)
+				assert.NotEqual(t, nil, err)
+				assert.Equal(t, test.ExpectedError, err.Error())
 			}
-
+			test.After()
 		})
 	}
 
